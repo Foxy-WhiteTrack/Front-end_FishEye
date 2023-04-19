3f2a2d159d780c35cc7b814391e92a7d0e8a9572
Foxy WhiteTrack | One Trick <foxywhitetrack@protonmail.com>
2023-04-19 13:53:24 +0200

    Implémentation de l'affichage des Medias du photographe
    Les images correspondant au photographe sont chargées sur la page photographer

--- scripts/pages/photographer.js
+++ scripts/pages/photographer.js
@@ -7,11 +7,20 @@ function getPhotographerIdFromUrl() {
 }
 
 // Fonction pour récupérer le photographe correspondant à l'ID
-async function getPhotographerById(id) {
+async function fetchData() {
     const response = await fetch("data/photographers.json");
     const data = await response.json();
+    return data;
+}
+
+function getPhotographerById(data, id) {
     const photographers = data.photographers;
     return photographers.find(photographer => photographer.id === id);
+}
+
+function getPhotographerMedia(data, photographerId) {
+    const media = data.media;
+    return media.filter(item => item.photographerId === photographerId);
 }
 
 // Fonction pour afficher les informations du photographe
@@ -25,12 +34,42 @@ function displayPhotographerInfo(photographer) {
     document.querySelector('#photograph_infos > p:nth-child(3)').innerHTML = photographer.tagline;
     document.querySelector('#img_photograph').innerHTML = `<img src="assets/images/Photographers ID Photos/${photo}" alt="Photo du profil de ${name}">`;
 }
+// Fonction pour afficher les médias du photographe
+function displayMedia(photographer, media) {
+    const mediaContainer = document.querySelector("#media_container");
+    media.forEach(item => {
+        const firstName = photographer.name.split(' ')[0]; // Récupérer le prénom du photographe
+        const mediaFolder = firstName; // Utiliser le prénom pour le nom du dossier
+
+        if (item.image) {
+            const imgPath = `assets/images/${mediaFolder}/${item.image}`;
+            mediaContainer.innerHTML += `
+                <div class="media_item">
+                    <img src="${imgPath}" alt="${item.title}">
+                </div>
+            `;
+        } else if (item.video) {
+            const videoPath = `assets/images/${mediaFolder}/${item.video}`;
+            mediaContainer.innerHTML += `
+                <div class="media_item">
+                    <video src="${videoPath}" controls></video>
+                </div>
+            `;
+        }
+    });
+}
+
+
 
 // Fonction principale pour récupérer et afficher les informations du photographe
 async function init() {
     const photographerId = getPhotographerIdFromUrl();
-    const photographer = await getPhotographerById(photographerId);
+    const data = await fetchData();
+    const photographer = getPhotographerById(data, photographerId);
+    const photographerMedia = getPhotographerMedia(data, photographerId);
+
     displayPhotographerInfo(photographer);
+    displayMedia(photographer, photographerMedia);
 }
 
 init();
